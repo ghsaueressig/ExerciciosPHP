19fc7883cc3eb9c9625484cf4a915dda2916e16a
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2022-03-14 16:40:20 -0300

    Exercícios PHP Capitulo 1 a 3

---
+++ operacoes.php
@@ -0,0 +1,11 @@
+<?php 
+
+$idade = 25;
+$idadeemdezanos = $idade + 10;
+
+echo $idadeemdezanos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2*2;
+$divisao = 2 /2;
---
+++ tipos.php
@@ -0,0 +1,9 @@
+<?php
+
+$idade = 21;
+$salario = 1000.301;
+$divisao = 10/3;
+
+$texto = "olá mundo";
+
+echo gettype($texto);
