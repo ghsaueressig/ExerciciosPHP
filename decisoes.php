e5e19fb7b17305faa2f5f0b8be31acc562fbc99d
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2022-03-15 14:31:45 -0300

    Exercicios Capitulo 4

--- decisoes.php
+++ decisoes.php
@@ -16,3 +16,11 @@ if ($idade >= 18) {
 
 echo PHP_EOL;
 echo "Adeus!";
+
+// $variavel = $condicao ? $valorSeVerdadeiro : $valorSeFalso;
+/*
+<?php
+$idade = 15;
+$mensagem = $idade < 18 ? ‘Você é menor de idade’ : ‘Você é maior de idade’;
+echo $mensagem; 
+*/
