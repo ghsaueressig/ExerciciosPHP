3547f28149079b9dc0351c213dedebce3d1e4eb2
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2022-03-17 15:01:53 -0300

    Curso PHP Avançado Atualizações

--- avançando/banco.php
+++ avançando/banco.php
@@ -20,8 +20,12 @@ $contasCorrentes = [
 $contasCorrentes['123.256.789-12'] = depositar($contasCorrentes['123.256.789-12'],900);
 
 $contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'],500);
+
 $contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'], 200);
 
+titularComLetrasMaiusculas($contasCorrentes['123.256.789-12']);
+
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem("$cpf {$conta['titular']} {$conta['saldo']}");
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    exibeMensagem("$cpf $titular  $saldo");
 }
--- avançando/listas.php
+++ avançando/listas.php
@@ -4,5 +4,10 @@
 $idadeList = [21, 23, 19, 25, 30, 41, 18];
 $umaIdade = $idadeList[2];
 
-
 echo "$umaIdade";
+
+$idadeVinicius = $idadeList[0];
+$idadeJoao = $idadeList[1];
+$idadeMaria = $idadeList[2];
+
+list($idadeVinicius, $idadeJoao, $idadeMaria) = $idadeList;
