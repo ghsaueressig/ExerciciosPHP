05b008725d3015f7f2ece7655aa0ee0e791063f5
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2023-03-16 21:34:00 -0300

    Update strings.php

--- strings.php
+++ strings.php
@@ -1,5 +1,4 @@
 <?php
-
 $idade = 18;
 
 echo "Olá mundo!" . PHP_EOL;
@@ -7,6 +6,16 @@ echo "Eu tenho \"$idade\" anos";
 
 echo PHP_EOL . PHP_EOL;
 
-echo "Você só pode entrar, se tiver mais de 18 anos\n";
+echo "Você só pode entrar se tiver mais de 18 anos!\n";
 
-echo "Você tem $idade anos. Pode entrar";
+echo PHP_EOL
+if ($idade >= 18)
+{
+    echo "Você tem $idade anos. Pode entrar!";
+}
+else
+{
+    echo "Você tem menos de 18 anos. Você não pode entrar!";
+}
+
+?>
